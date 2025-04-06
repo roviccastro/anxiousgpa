0f72ce2313e78df02ce79605dbf2015e074eb4a3
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-04-06 21:46:17 +0800

    Create add row function
    - Created a pre-generated Course Name,
    Unit(s), Grade, text and number input
    in HTML

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const generate_btn = document.querySelector("#generate-btn")
-const calculate_btn = document.querySelector("#calculate-btn")
+const body = document.querySelector(".body")
+const container = document.querySelector(".calculator-container");
 
+const course_info = document.querySelector(".row");
+const course_input = document.querySelector(".course-name");
+const course_unit = document.querySelector(".course-unit");
+const course_grade = document.querySelector(".course-grade");
+
+const generate_btn = document.querySelector("#generate-btn");
+const calculate_btn = document.querySelector("#calculate-btn");
+
+function createCalculator(){
+  let one_row = course_info.cloneNode(true);
+  container.appendChild(one_row);
+}
+
+generate_btn.addEventListener('click', createCalculator)
